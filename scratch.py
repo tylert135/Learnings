f30077b6355555c2727f3143393bb34fb7685fff
tylert135 <tylert@Mac.hsd1.pa.comcast.net>
2026-05-13 23:27:27 -0400

    Initial learning project

---
+++ scratch.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(
+        headless=False
+    )
+
+    page = browser.new_page()
+
+    page.goto("https://example.com")
+
+    print(page.title())
+
+    input("Press ENTER to close browser...")
+
+    browser.close()
