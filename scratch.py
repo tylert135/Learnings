83a51a686e5074c8058f27f400d2b7b5d7c10be7
tylert135 <tylert135@gmail.com>
2026-05-14 02:24:26 -0400

    Using Codegen

--- scratch.py
+++ scratch.py
@@ -1,17 +1,22 @@
-from playwright.sync_api import sync_playwright
+import re
+import time
+from playwright.sync_api import Playwright, sync_playwright, expect
 
-with sync_playwright() as p:
 
-    browser = p.chromium.launch(
-        headless=False
-    )
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.stpaulsbc.com/")
+    page.get_by_role("link", name="Contact").click()
 
-    page = browser.new_page()
-
-    page.goto("https://example.com")
-
-    print(page.title())
-
-    input("Press ENTER to close browser...")
-
+    # Place this right before browser.close() or context.close()
+    page.wait_for_timeout(5000)  # Time is in milliseconds
+    
+    # ---------------------
+    context.close()
     browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
---
+++ test.py
@@ -0,0 +1,11 @@
+# Get input from the user for the first number
+num1 = 5
+
+# Get input from the user for the second number
+num2 = 10
+
+# Add the two numbers
+sum_result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {sum_result}")
